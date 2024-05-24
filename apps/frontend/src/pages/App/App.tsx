7943d7e8876240e6c2e4b514f530cd18653aa601
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-05-24 05:50:27 -0700

    feat(apps): demonstrate communication between frontend and api

--- apps/frontend/src/pages/App/App.tsx
+++ apps/frontend/src/pages/App/App.tsx
@@ -1,8 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+
 import './App.scss';
 
+const apiHealthEndpoint = (new URL('/healthz', import.meta.env.VITE_API)).toString();
+
 function App() {
+  const healthQuery = useQuery({
+    queryKey: ['api_health'],
+    queryFn: () => fetch(apiHealthEndpoint),
+  });
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <p>API Status: {healthQuery.data?.status === 200 ? 'Online' : 'Offline'}</p>
+    </>
   );
 }
 
