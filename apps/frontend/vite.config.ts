ef1a90deb82c7634e0b7d4345d82ddc939741a7f
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-05-26 00:42:20 -0700

    feat(apps): add port environment variables

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,12 +5,15 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig(({ mode }) => {
   const envDir = join('..', '..');
-  const env = loadEnv(mode, envDir, ['PROXY', 'VITE']);
+  const env = loadEnv(mode, envDir, ['PROXY', 'VITE', 'WEB_PORT']);
+
+  const PORT = parseInt(env.WEB_PORT, 10);
 
   return {
     plugins: [react()],
     envDir,
     server: {
+      port: PORT,
       proxy: {
         '/api': {
           target: env.PROXY_API,
