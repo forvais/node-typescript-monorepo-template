f70a5d6056794312ac7c707cc3eda1b343d157fd
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-05-25 20:22:53 -0700

    feat(frontend): configure api proxy

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite';
+import { join } from 'path';
+
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  plugins: [react()],
-  envDir: '../..',
+export default defineConfig(({ mode }) => {
+  const envDir = join('..', '..');
+  const env = loadEnv(mode, envDir, ['PROXY', 'VITE']);
+
+  return {
+    plugins: [react()],
+    envDir,
+    server: {
+      proxy: {
+        '/api': {
+          target: env.PROXY_API,
+          changeOrigin: true,
+          rewrite: path => {
+            if (path.toLowerCase() === '/api/healthz') {
+              return '/healthz';
+            }
+
+            return path;
+          },
+        },
+      },
+    },
+  };
 });
