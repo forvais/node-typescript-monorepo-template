b79fe31fe6e2bf69705021e37fcca6bc75159dad
Benjamin Chiang <chiangbenjamin991@gmail.com>
2025-06-02 18:41:14 -0700

    feat(frontend): implement 'raise' example usage

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { raise } from '@node-typescript-monorepo-template/utils';
+
 import App from './pages/App/App.tsx';
 import './index.scss';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const container = document.getElementById('root')!;
+const container = document.getElementById('root') ?? raise(new Error('Could not find root element.'));
 const root = ReactDOM.createRoot(container);
 
 const queryClient = new QueryClient();
